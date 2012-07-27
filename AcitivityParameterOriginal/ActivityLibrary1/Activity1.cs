6cdd147320ca8c43fa3a1190ff218a2518f74cdd
Edward.Zhan <zxhlelva@126.com>
2012-07-27 13:07:12 +0800

    Initiate Project
    Initiate Project

---
+++ AcitivityParameterOriginal/ActivityLibrary1/Activity1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Collections;
+using System.Drawing;
+using System.Linq;
+using System.Workflow.ComponentModel;
+using System.Workflow.ComponentModel.Design;
+using System.Workflow.ComponentModel.Compiler;
+using System.Workflow.ComponentModel.Serialization;
+using System.Workflow.Runtime;
+using System.Workflow.Activities;
+using System.Workflow.Activities.Rules;
+
+namespace ActivityLibrary1
+{
+	public partial class Activity1: Activity
+	{
+		public Activity1()
+		{
+			InitializeComponent();
+		}
+
+        protected override ActivityExecutionStatus Execute(ActivityExecutionContext executionContext)
+        {
+            return ActivityExecutionStatus.Closed;
+        }
+	}
+}
